f091a4ea5a888a6230741bd97720efacef91eca4
Vsevolod Romashov <7@7vn.ru>
2015-02-23 22:52:45 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'rake'
+
+gem 'lotusrb',     '0.2.1'
+gem 'lotus-model', '~> 0.2'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+end
+
+group :production do
+  gem 'puma'
+end
---
+++ lib/keeper.rb
@@ -0,0 +1,37 @@
+require 'lotus/model'
+Dir["#{ __dir__ }/keeper/**/*.rb"].each { |file| require_relative file }
+
+Lotus::Model.configure do
+  # Database adapter
+  #
+  # Available options:
+  #
+  #  * Memory adapter
+  #    adapter type: :memory, uri: 'memory://localhost/keeper_development'
+  #
+  #  * SQL adapter
+  #    adapter type: :sql, uri: 'sqlite://db/keeper_development.db'
+  #    adapter type: :sql, uri: 'postgres://localhost/keeper_development'
+  #    adapter type: :sql, uri: 'mysql://localhost/keeper_development'
+  #
+  adapter type: :file_system, uri: ENV['KEEPER_DATABASE_URL']
+
+  ##
+  # Database mapping
+  #
+  # You can specify mapping file to load with:
+  #
+  # mapping "#{__dir__}/config/mapping"
+  #
+  # Alternatively, you can use a block syntax like the following:
+  #
+  mapping do
+    # collection :users do
+    #   entity     User
+    #   repository UserRepository
+    #
+    #   attribute :id,   Integer
+    #   attribute :name, String
+    # end
+  end
+end.load!
