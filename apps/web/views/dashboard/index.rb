8a189aba3eeebcb885280699a719ab85ff4bafa2
Vsevolod Romashov <7@7vn.ru>
2015-03-04 01:19:56 +0300

    view helpers

--- apps/web/views/dashboard/index.rb
+++ apps/web/views/dashboard/index.rb
@@ -1,5 +1,13 @@
 module Web::Views::Dashboard
   class Index
     include Web::View
+    
+    def format_time(time)
+      time.strftime('%b %e %H:%M')
+    end
+    
+    def time_to_words(time)
+      ::TimeLord::Time.new(time).period.to_words
+    end
   end
 end
